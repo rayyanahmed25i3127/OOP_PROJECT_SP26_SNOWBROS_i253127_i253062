185b5c47429ac6019a1956e297174edd54de9ed0
Rayyan Ahmed <i253127@isb.nu.edu.pk>
2026-04-19 02:01:10 +0500

    setup: SFML 3 starter window and CMake build config

---
+++ build/CMakeFiles/snow-bros.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snow-bros.dir/src/main.cpp.o"
+  "CMakeFiles/snow-bros.dir/src/main.cpp.o.d"
+  "snow-bros"
+  "snow-bros.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snow-bros.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/snow-bros.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for snow-bros.
+# This may be replaced when dependencies are built.
