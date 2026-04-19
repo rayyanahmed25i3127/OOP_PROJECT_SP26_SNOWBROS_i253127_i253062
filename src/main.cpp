0621ebedcf96b4ac3232b092403275e9de0f9282
MuhammadAnas-25i3062 <i253062@isb.nu.edu.pk>
2026-04-19 16:06:56 +0500

    Removed build folder from repo

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,45 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
 
-int main() 
+int main()
 {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Snow Bros");
-    
-    while (window.isOpen()) 
+    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 600)), "Snow Bros");
+
+    // Player
+    sf::CircleShape player(25.f);
+    player.setFillColor(sf::Color::Green);
+    player.setPosition(sf::Vector2f(400.f, 500.f));
+
+    float speed = 0.5f;
+
+    std::vector<sf::CircleShape> enemies;
+
+    while (window.isOpen())
     {
-        while (const std::optional event = window.pollEvent()) 
+        while (auto event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
-        
-        window.clear(sf::Color::Black);
+
+        // Movement (SFML 3)
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+            player.move(sf::Vector2f(-speed, 0));
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+            player.move(sf::Vector2f(speed, 0));
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+            player.move(sf::Vector2f(0, -speed));
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+            player.move(sf::Vector2f(0, speed));
+
+        // Render
+        window.clear();
+        window.draw(player);
         window.display();
     }
-    
+
     return 0;
 }
