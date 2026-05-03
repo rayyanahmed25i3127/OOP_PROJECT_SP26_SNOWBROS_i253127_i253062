cf9269e3d8756fd2899e96611aef15392304908e
MuhammadAnas-25i3062 <i253062@isb.nu.edu.pk>
2026-05-04 05:22:42 +1000

    Solved date issue and added level reached

--- src/Leaderboard.cpp
+++ src/Leaderboard.cpp
@@ -13,56 +13,52 @@ void Leaderboard::loadFromFile(const std::string& filename) {
     }
 
     m_count = 0;
+    char line[200];
 
-    std::string name, date;
-    int score, level;
-//(mlf:for spacing):
-char line[200];
+    while (file.getline(line, 200)) {
+        if (m_count >= MAX_ENTRIES) break;
 
-while (file.getline(line, 200)) {
-    if (m_count >= MAX_ENTRIES) break;
+        char name[100];
+        int score, level;
+        char date[20];
 
-    char name[100];
-    int score, level;
-    char date[20];
+        // Fixed: use commas to match save format: name,score,level,date
+        sscanf(line, "%[^,],%d,%d,%s", name, &score, &level, date);
 
-    sscanf(line, "%[^,],%d %d %s", name, &score, &level, date);
+        m_entries[m_count].playerName = name;
+        m_entries[m_count].score = score;
+        m_entries[m_count].levelReached = level;  // Fixed: actually store level
+        m_entries[m_count].date = date;
 
-    m_entries[m_count].playerName = name;
-    m_entries[m_count].score = score;
-    m_entries[m_count].date = date;
-
-    m_count++;
-}
+        m_count++;
+    }
    
-    //mlf:(sorting)
+    // Sort by score descending
     for (int i = 0; i < m_count - 1; i++) {
-    for (int j = 0; j < m_count - i - 1; j++) {
-        if (m_entries[j].score < m_entries[j + 1].score) {
-
-            // swap entries
-            LeaderboardEntry temp = m_entries[j];
-            m_entries[j] = m_entries[j + 1];
-            m_entries[j + 1] = temp;
+        for (int j = 0; j < m_count - i - 1; j++) {
+            if (m_entries[j].score < m_entries[j + 1].score) {
+                LeaderboardEntry temp = m_entries[j];
+                m_entries[j] = m_entries[j + 1];
+                m_entries[j + 1] = temp;
+            }
         }
     }
-}
-//(MLF:LMIT of 10):
-if (m_count > 10) {
-    m_count = 10;
-}
 
-std::ofstream outFile(filename);
+    // Limit to top 10
+    if (m_count > 10) {
+        m_count = 10;
+    }
 
-for (int i = 0; i < m_count; i++) {
-   outFile << m_entries[i].playerName << ","
-        << m_entries[i].score << " "
-        << 1 << " "
-        << m_entries[i].date << "\n";
-}
-
-outFile.close();
-
+    // Write back sorted top 10
+    std::ofstream outFile(filename);
+    for (int i = 0; i < m_count; i++) {
+        // Fixed: use commas and actual level value
+        outFile << m_entries[i].playerName << ","
+                << m_entries[i].score << ","
+                << m_entries[i].levelReached << ","
+                << m_entries[i].date << "\n";
+    }
+    outFile.close();
     file.close();
 }
 
